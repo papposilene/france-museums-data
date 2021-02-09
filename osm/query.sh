f5a183213cc57cc5f6f686fdd1494bb3ac248c68
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-02-09 09:55:38 +0100

    Update query.sh

--- osm/query.sh
+++ osm/query.sh
@@ -4,4 +4,17 @@ osmosis \
  --tf accept-nodes tourism=museum \
  --tf reject-ways \
  --tf reject-relations \
- --write-xml data/all-museums.osm
+ --write-xml data/output-museums-nodes.osm
+ 
+osmosis \
+ --read-xml data/planet-170102.osm \
+ --tf accept-ways tourism=museum \
+ --tf reject-relations \
+ --tf --used-node \
+ --write-xml data/output-museums-ways.osm
+ 
+osmosis \
+ --rx data/output-museums-ways.osm \
+ --rx data/output-museums-nodes \
+ --merge \
+ --wx data/output-museums-merged.osm
