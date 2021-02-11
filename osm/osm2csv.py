3cddcc02f01560d9e2213b8124eaf91ce6f8f45c
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-02-11 19:32:05 +0100

    OSM: final python and final CSV files

--- osm/osm2csv.py
+++ osm/osm2csv.py
@@ -27,10 +27,6 @@ def create_entry():
     return {
         "osm_id": None,
         "name": None,
-        "name:en": None,
-        "int_name": None,
-        "old_name": None,
-        "old_name:en": None,
         "number": None,
         "street": None,
         "postal_code": None,
@@ -56,7 +52,7 @@ def main():
 
     with open(args.output, 'wb') as csv_file:
 
-        fieldnames = ['osm_id', 'name', 'name:en', 'int_name', 'old_name', 'old_name:en', 'number', 'street', 'postal_code',
+        fieldnames = ['osm_id', 'name', 'number', 'street', 'postal_code',
                       'city', 'country', 'country_code', 'lat', 'lon', 'website', 'email', 'phone', 'fax', 'tags', 'description', 'date_added',
                       'wikidata']
         csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
@@ -68,22 +64,24 @@ def main():
         http = urllib3.PoolManager()
         tree = ET.parse(args.input)
         root = tree.getroot()
-        
+
+        # Node
         for node in root.findall('node'):
             print(f"{bcolors.OKGREEN}Row #", num_rows, f"{bcolors.ENDC}")
             print(f"{bcolors.OKCYAN}Node :", node, f"{bcolors.ENDC}")
-            
-            for tag in root.findall('tag'):
-                print(f"{bcolors.OKCYAN}Tag :", tag, f"{bcolors.ENDC}")
 
-                entry['osm_id'] = node.get('id')
-                entry['date_added'] = node.get('timestamp')
+            if node.find('tag') is None:
+                continue
 
-                osm_url = http.request('GET', 'https://nominatim.openstreetmap.org/lookup?format=jsonv2&addressdetails=1&extratags=1&namedetails=1&osm_ids=N' + node.get('id'))
-                osm_data = json.loads(osm_url.data.decode('utf-8'))
+            entry['osm_id'] = node.get('id')
+            entry['date_added'] = node.get('timestamp')
 
-                print(osm_data)
-                entry['name'] = osm_data[0]['namedetails']['name']
+            osm_url = http.request('GET', 'https://nominatim.openstreetmap.org/lookup?format=jsonv2&addressdetails=1&extratags=1&namedetails=1&osm_ids=N' + node.get('id'))
+            osm_data = json.loads(osm_url.data.decode('utf-8'))
+
+            print(osm_data)
+            if (len(osm_data) > 0):
+                if 'name' in osm_data[0]['namedetails']: entry['name'] = osm_data[0]['namedetails']['name']
 
                 entry['lat'] = osm_data[0]['lat']
                 entry['lon'] = osm_data[0]['lon']
@@ -102,22 +100,70 @@ def main():
                 if 'country' in osm_data[0]['address']: entry['country'] = osm_data[0]['address']['country']
                 if 'country_code' in osm_data[0]['address']: entry['country_code'] = osm_data[0]['address']['country_code']
 
-                if 'k' in tag and tag['k'] == 'website': entry['website'] = tag.find('v').text
-                if 'k' in tag and tag['k'] == 'email': entry['email'] = tag.find('v').text
-                if 'k' in tag and tag['k'] == 'phone': entry['phone'] = tag.find('v').text
-                if 'k' in tag and tag['k'] == 'wikidata': entry['wikidata'] = tag.find('v').text
-                if 'k' in tag and tag['k'] == 'description': entry['description'] = tag.find('v').text
+            for tag in node.findall('tag'):
+                print(tag.get('v'))
+                if tag.get('k') == 'website': entry['website'] = tag.get('v')
+                if tag.get('k') == 'email': entry['email'] = tag.get('v')
+                if tag.get('k') == 'phone': entry['phone'] = tag.get('v')
+                if tag.get('k') == 'wikidata': entry['wikidata'] = tag.get('v')
+                if tag.get('k') == 'description': entry['description'] = tag.get('v')
 
-                entry['tags'] = 'osm:museum'
-
-                # add to csv
-                csv_writer.writerow(entry)
-
-            else:
-                print('Skipped')
+            entry['tags'] = 'osm:museum'
 
             num_rows += 1
+            # add to csv
+            csv_writer.writerow(entry)
             entry = create_entry()
+
+        # Ways
+        for node in root.findall('way'):
+            print(f"{bcolors.OKGREEN}Row #", num_rows, f"{bcolors.ENDC}")
+            print(f"{bcolors.OKCYAN}Node :", node, f"{bcolors.ENDC}")
+
+            if node.find('tag') is None:
+                continue
+
+            entry['osm_id'] = node.get('id')
+            entry['date_added'] = node.get('timestamp')
+
+            osm_url = http.request('GET', 'https://nominatim.openstreetmap.org/lookup?format=jsonv2&addressdetails=1&extratags=1&namedetails=1&osm_ids=W' + node.get('id'))
+            osm_data = json.loads(osm_url.data.decode('utf-8'))
+
+            print(osm_data)
+            if (len(osm_data) > 0):
+                if 'name' in osm_data[0]['namedetails']: entry['name'] = osm_data[0]['namedetails']['name']
+                entry['lat'] = osm_data[0]['lat']
+                entry['lon'] = osm_data[0]['lon']
+                if 'road' in osm_data[0]['address']: entry['street'] = osm_data[0]['address']['road']
+                if 'postcode' in osm_data[0]['address']: entry['postal_code'] = osm_data[0]['address']['postcode']
+                if 'village' in osm_data[0]['address']:
+                    entry['city'] = osm_data[0]['address']['village']
+                elif 'town' in osm_data[0]['address']:
+                    entry['city'] = osm_data[0]['address']['town']
+                elif 'municipality' in osm_data[0]['address']:
+                    entry['city'] = osm_data[0]['address']['municipality']
+                elif 'city' in osm_data[0]['address']:
+                    entry['city'] = osm_data[0]['address']['city']
+                else:
+                    entry['city'] = ""
+                if 'country' in osm_data[0]['address']: entry['country'] = osm_data[0]['address']['country']
+                if 'country_code' in osm_data[0]['address']: entry['country_code'] = osm_data[0]['address']['country_code']
+
+            for tag in node.findall('tag'):
+                print(tag.get('v'))
+                if tag.get('k') == 'website': entry['website'] = tag.get('v')
+                if tag.get('k') == 'email': entry['email'] = tag.get('v')
+                if tag.get('k') == 'phone': entry['phone'] = tag.get('v')
+                if tag.get('k') == 'wikidata': entry['wikidata'] = tag.get('v')
+                if tag.get('k') == 'description': entry['description'] = tag.get('v')
+
+            entry['tags'] = 'osm:museum'
+
+            num_rows += 1
+            # add to csv
+            csv_writer.writerow(entry)
+            entry = create_entry()
+
         print('wrote {} rows to {}'.format(num_rows, args.output))
 
 if __name__ == '__main__':
